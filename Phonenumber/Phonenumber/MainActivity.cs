1120cf6ba367ad57ea8419667e5908e7c44ace30
pangian <pangianmain@gmail.com>
2019-04-03 16:55:40 +0300

    SIx

--- Phonenumber/Phonenumber/MainActivity.cs
+++ Phonenumber/Phonenumber/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Views;
 using Java.Lang.Ref;
 using System;
 using Android.Content;
+using System.Linq;
 
 namespace Phonenumber
 {
@@ -18,6 +19,9 @@ namespace Phonenumber
         EditText passwordField;
         String dataUser = " ";
         String dataPass = " ";
+        int requiredDigits = 8;
+        int requiredUppercase = 1;
+        int requiredNumbers = 1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -28,22 +32,28 @@ namespace Phonenumber
             passwordField = FindViewById<EditText>(Resource.Id.editTextPassword);
 
             loginButton.Click += SendData;
-            
+
         }
-        
-        public void SendData(object sender,EventArgs e)
+
+        public void SendData(object sender, EventArgs e)
         {
             dataUser = usernameField.Text;
             dataPass = passwordField.Text;
-            if (dataUser.Equals("") )
+            if (dataUser.Equals(""))
             {
-                Toast.MakeText(this,"Please Enter Username", ToastLength.Long).Show();
+                Toast.MakeText(this, "Please Enter Username", ToastLength.Long).Show();
             }
-            if (dataPass.Length <= 8 )
+            bool InvalidPass = dataPass.Where(Char.IsDigit).Count() <= requiredDigits &&
+                dataPass.Where(Char.IsUpper).Count() <= requiredUppercase &&
+                dataPass.Where(Char.IsNumber).Count() <= requiredNumbers;
+
+            if (InvalidPass == true)
             {
-                Toast.MakeText(this, "Your password bust contain more than 8 letters", ToastLength.Long).Show();
+                Toast.MakeText(this, "Your password bust contain more than 8 letters and more than 1 upper letter and number", ToastLength.Long).Show();
             }
-            else { Intent intent = new Intent(this,typeof(SecondActivity));
+            else
+            {
+                Intent intent = new Intent(this, typeof(SecondActivity));
                 intent.PutExtra("text", dataUser);
                 StartActivity(intent);
             }
@@ -51,4 +61,3 @@ namespace Phonenumber
         }
     }
 }
-      
